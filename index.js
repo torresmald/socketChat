56d3a98dab7639170a8a5468e9495a0846894f5f
Jonathan Torres <jonathan.torresmald@gmail.com>
2023-03-29 20:01:34 +0200

    railway

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParse = require('body-parser');
 const app = express();
 const htpp = require('http').Server(app);
+const PORT = process.env.PORT || 4000;
 const io = require('socket.io')(htpp, {
     cors: {
         origins: ["http://localhost:4200"],
@@ -9,7 +11,12 @@ const io = require('socket.io')(htpp, {
 
     }
 })
+
 app.use(cors());
+
+app.use(bodyParse.urlencoded({extended: true}))
+app.use(bodyParse.json());
+
 io.on('connection', (socket) => {
     console.log('Nuevo usuario Conectado');
     socket.on('sendMessage', (messageInfo) => {
@@ -21,8 +28,8 @@ io.on('connection', (socket) => {
 app.get('/', (request, response) => {
     response.status(200).json('Bienvenido a la API.')
 })
-htpp.listen(4000, () => {
-    console.log(`Listening in http://localhost:4000`);
+htpp.listen(PORT, () => {
+    console.log(`Escuchando en http://localhost:${PORT}`);
 });
 
 module.exports = app;
